8a3ecb7545e1e39b1ca7650cdee22c1d51e7abf9
Packiaseelan <packiaseelan14@gmail.com>
2019-12-02 22:16:52 +0530

    removed unused file

--- lib/Views/models/challenge_details.dart
+++
@@ -1,8 +0,0 @@
-// ChallengeDetailsModel{
-//   final int remainingDays;
-//   final double remainingAverage;
-//   // final double totalDistance;
-//   // final double coveredDistance;
-//   // final double averageCovered;
-//   ChallengeDetailsModel({this.remainingDays, this.remainingAverage });
-// }
